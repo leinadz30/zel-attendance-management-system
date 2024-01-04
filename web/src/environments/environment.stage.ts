f6243089a9b6ffc9a971ca656de67ea0943567d8
erwin-io <erwinramirez220@gmail.com>
2024-01-05 02:09:52 +0800

    updates

--- web/src/environments/environment.stage.ts
+++ web/src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiBaseUrl: "https://zams-api.vercel.app/api/v1",
+  production: false,
+  apiBaseUrl: "https://zams-stage-api.vercel.app/api/v1",
   pusher: {
     key: "1ad4b93854243ae307e6",
     cluster: "ap1",
