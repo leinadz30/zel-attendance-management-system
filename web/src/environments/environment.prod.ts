fd009708be52c2b426f6c640bdb68870f1f5e460
erwin-io <erwinramirez220@gmail.com>
2024-03-10 22:38:09 +0800

    environment variable

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://zams-api.vercel.app/api/v1",
+  apiBaseUrl: "https://zams.vercel.app/api/v1",
   pusher: {
     key: "1ad4b93854243ae307e6",
     cluster: "ap1",
-  }
+  },
+  oneSignalAppId: "e72b9652-3594-4bd1-8f58-d01fc50d0b11"
 };
